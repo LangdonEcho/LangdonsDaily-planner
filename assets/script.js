aca85c711d6873068621694a9fcd1ab2a0cba4b0
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-11-14 22:10:06 -0600

    working on API implementation

--- assets/script.js
+++ assets/script.js
@@ -6,64 +6,119 @@
  // saveButton click listener function
 
 
-$(".hour7").on("click", function() {
+$("#hour7").on("click", function() {
   var description = $(".h7").val();
-  $("#hour7 .description").val(localStorage.getItem("hour7"))
+  localStorage.setItem("7 AM", description)
 });
+$("#hour7").val(localStorage.getItem("7 AM"));
 
-$(".hour8").on("click", function() {
-  var description = $(".h8").val();
-  $("#hour8 .description").val(localStorage.getItem("hour8"))
+$("#hour8").on("click", function() {
+  var description = $(".h8").val()
+  localStorage.setItem("8 AM", description)
 });
+$("#hour8").val(localStorage.getItem("8 AM"))
 
-$(".hour9").on("click", function() {
+$("#hour9").on("click", function() {
   var description = $(".h9").val();
-  $("#hour9 .description").val(localStorage.getItem("hour9"))
+  localStorage.setItem("hour9", description)
 });
+$("#hour9").val(localStorage.getItem("9 AM"))
 
 $(".hour10").on("click", function() {
   var description = $(".h10").val();
-  $("#hour10 .description").val(localStorage.getItem("hour10"))
+  localStorage.setItem("hour10", description)
 });
+$("#hour10").val(localStorage.getItem("10 AM"))
 
 $(".hour11").on("click", function() {
   var description = $(".h11").val();
-  $("#hour11 .description").val(localStorage.getItem("hour11"))
+  localStorage.setItem("hour11", description)
 });
+$("#hour11").val(localStorage.getItem("11 AM"))
 
 $(".hour12").on("click", function() {
   var description = $(".h12").val();
-  $("#hour12 .description").val(localStorage.getItem("hour12"))
-});
+ localStorage.setItem("hour12", description)
+}); 
+$("#hour12").val(localStorage.getItem("12 AM"))
 
-$(".hour1").on("click", function() {
+$(".hour13").on("click", function() {
   var description = $(".h1").val();
-  $("#hour1 .description").val(localStorage.getItem("hour1"))
+  localStorage.setItem("hour1", description)
 });
+$("#hour13").val(localStorage.getItem("1 PM"))
 
-$(".hour2").on("click", function() {
+$(".hour14").on("click", function() {
   var description = $(".h2").val();
-  $("#hour2 .description").val(localStorage.getItem("hour2"))
+  localStorage.setItem("hour2", description)
 });
+$("#hour14").val(localStorage.getItem("2 PM"))
 
-$(".hour3").on("click", function() {
+$(".hour15").on("click", function() {
   var description = $(".h3").val();
-  $("#hour3 .description").val(localStorage.getItem("hour3"))
+  localStorage.getItem("hour3", description)
 });
+$("#hour15").val(localStorage.getItem("3 PM"))
     // Save text in local storage
     localStorage.setItem(time, text);
 
+    //Gets the current time and turns the current hour into a number.
+var currentTime = moment().format('H');
+currentTime = parseInt(currentTime);
 
-    // Get item from local storage if any
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
+//Getting the hours from the page and converting them into numbers.
+//Also getting each texarea.
+var hr7 = document.querySelector(".hr7").textContent;
+hr7 = parseInt(hr7[0]);
+var h7 = document.querySelector(".h7");
 
-    timeTracker();
+var hr8 = document.querySelector(".hr8").textContent;
+hr8 = parseInt(hr8[1]) + 10;
+var h8 = document.querySelector(".h8");
+
+var hr9 = document.querySelector(".hr9").textContent;
+hr9 = parseInt(hr9[1]) +10;
+var e11 = document.querySelector(".h9");
+
+var hr10 = document.querySelector(".hr10").textContent;
+hr10 = parseInt(hr10[1]) + 10;
+var h10 = document.querySelector(".h10");
+
+var hr11 = document.querySelector(".hr11").textContent;
+hr11 = parseInt(hr11[0]) +12;0
+h11 = document.querySelector(".h11");
+
+var hr12 = document.querySelector(".hr12").textContent;
+hr12 = parseInt(t6[0]) +12;
+var h12 = document.querySelector(".h12");
+
+var hr1 = document.querySelector(".h1").textContent;
+hr1 = parseInt(t7[0]) +12;
+var h1 = document.querySelector(".h1");
+
+var hr2 = document.querySelector(".hr2").textContent;
+hr2 = parseInt(t8[0]) +12;
+var h2 = document.querySelector(".h2");
+
+var hr3 = document.querySelector(".hr3").textContent;
+hr3 = parseInt(t9[0]) +12;
+var h3 = document.querySelector(".h3");
+
+
+//function that color codes the various time rows depending on current hour.
+var hours  = [h7, h8, h9, h10, h11, h12, h1, h2, h3];
+var inputs = [hr7, hr8, hr9, hr10, hr11, hr12, hr1, hr2, hr3];
+function colorcoding() {
+    for (i = 0; i < hours.length; i ++) {
+        if (hours[i] < currentTime) {
+            inputs[i].classList.add("past");
+        }
+        else if (hours[i] === currentTime) {
+            inputs[i].classList.add("present");
+        }
+        else {
+            inputs[i].classList.add("future");
+        }
+    }
+}
+colorcoding();
